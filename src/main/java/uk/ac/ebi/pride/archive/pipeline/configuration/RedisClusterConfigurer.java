405bd1afbfbb3ea249907d0d2194ec351e8f5853
Tobias-Ternent <tobias@ebi.ac.uk>
2018-05-08 11:47:10 +0100

    Refactored Redis config out to separate classes.

---
+++ src/main/java/uk/ac/ebi/pride/archive/pipeline/configuration/RedisClusterConfigurer.java
@@ -0,0 +1,93 @@
+package uk.ac.ebi.pride.archive.pipeline.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@Slf4j
+@EnableBatchProcessing
+@ComponentScan(basePackageClasses = {DefaultBatchConfigurer.class})
+public class RedisClusterConfigurer {
+
+  @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+  @Autowired
+  private StepBuilderFactory stepBuilderFactory;
+
+  @Value("${redis.host}")
+  private String redisServer;
+
+  @Value("${redis.port}")
+  private String redisPort;
+
+  private JedisCluster jedisCluster;
+
+  /** Creates a new Jedis pool if one has yet to be created yet. */
+  @Bean
+  public Step createJedisCluster() {
+    return stepBuilderFactory
+        .get("createJedisCluster")
+        .tasklet(
+            (stepContribution, chunkContext) -> {
+              Set<HostAndPort> jedisClusterNodes = new HashSet<>();
+              final String STRING_SEPARATOR = "##";
+              if (redisServer.contains(STRING_SEPARATOR)) {
+                String[] servers = redisServer.split(STRING_SEPARATOR);
+                String[] ports;
+                if (redisPort.contains(STRING_SEPARATOR)) {
+                  ports = redisPort.split(STRING_SEPARATOR);
+                } else {
+                  ports = new String[] {redisPort};
+                }
+                if (ports.length != 1 && ports.length != servers.length) {
+                  log.error(
+                      "Mismatch between provided Redis ports and servers. Should either have 1 port for all servers, or 1 port per server");
+                }
+                for (int i = 0; i < servers.length; i++) {
+                  String serverPort = ports.length == 1 ? ports[0] : ports[i];
+                  jedisClusterNodes.add(new HostAndPort(servers[i], Integer.parseInt(serverPort)));
+                  log.info("Added Jedis node: " + servers[i] + " " + serverPort);
+                }
+              } else {
+                jedisClusterNodes.add(new HostAndPort(redisServer, Integer.parseInt(redisPort)));
+                // Jedis Cluster will attempt to discover cluster nodes automatically
+                log.info("Added Jedis node: " + redisServer + " " + redisPort);
+              }
+              final JedisPoolConfig DEFAULT_CONFIG = new JedisPoolConfig();
+              jedisCluster = new JedisCluster(jedisClusterNodes, DEFAULT_CONFIG);
+              return RepeatStatus.FINISHED;
+            })
+        .build();
+  }
+
+  /**
+   * Sets new jedisCluster.
+   *
+   * @param jedisCluster New value of jedisCluster.
+   */
+  public void setJedisCluster(JedisCluster jedisCluster) {
+    this.jedisCluster = jedisCluster;
+  }
+
+  /**
+   * Gets jedisCluster.
+   *
+   * @return Value of jedisCluster.
+   */
+  public JedisCluster getJedisCluster() {
+    return jedisCluster;
+  }
+}
---
+++ src/main/java/uk/ac/ebi/pride/archive/pipeline/utility/RedisUtilities.java
@@ -0,0 +1,51 @@
+package uk.ac.ebi.pride.archive.pipeline.utility;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.JedisCluster;
+
+@Slf4j
+public class RedisUtilities {
+
+  /**
+   * Gets a String value for the specified Redis key.
+   *
+   * @param key the key to use
+   * @return String the key's value
+   */
+  public static String getStringFromRedis(String key, JedisCluster jedisCluster) {
+	String result = "";
+	try {
+	  log.info("Connecting to Redis.");
+	  log.info("Getting contents to Redis for key: " + key);
+	  if (!jedisCluster.exists(key)) {
+		log.error("Redis does not hold a value for key: " + key);
+	  } else {
+		result = jedisCluster.get(key);
+		log.info("Successfully gotten value from key: " + result);
+	  }
+	} catch (Exception e) {
+	  log.error("Exception while getting value to Redis for key: " + key, e);
+	}
+	return result;
+  }
+
+  /**
+   * Removes a key (or more) and the corresponding value(s) from Redis.
+   *
+   * @param keyToRemove the key to remove
+   */
+  public static void removeKeyAndValueInRedis(String keyToRemove, JedisCluster jedisCluster) {
+	try {
+	  log.info("Connecting to Redis.");
+	  log.info("Removing Redis key: " + keyToRemove);
+	  long numberOfKeysRemoved = jedisCluster.del(keyToRemove);
+	  if (numberOfKeysRemoved < 1) {
+		log.info("Redis does not hold a value for key: " + keyToRemove);
+	  } else {
+		log.info("Successfully removed key: " + keyToRemove);
+	  }
+	} catch (Exception e) {
+	  log.error("Exception while removing in Redis for key: " + keyToRemove, e);
+	}
+  }
+}
