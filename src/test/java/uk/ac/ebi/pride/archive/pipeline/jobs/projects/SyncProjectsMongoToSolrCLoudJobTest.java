1d3f389f236378d46158870a8d304718ec0adec3
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-06-14 10:23:04 +0100

    test added

---
+++ src/test/java/uk/ac/ebi/pride/archive/pipeline/jobs/projects/SyncProjectsMongoToSolrCLoudJobTest.java
@@ -0,0 +1,57 @@
+package uk.ac.ebi.pride.archive.pipeline.jobs.projects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.pride.archive.pipeline.configuration.JobRunnerTestConfiguration;
+import uk.ac.ebi.pride.archive.pipeline.utility.SubmissionPipelineConstants;
+
+/**
+ * This code is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * ==Overview==
+ * <p>
+ * This class
+ * <p>
+ * Created by ypriverol (ypriverol@gmail.com) on 14/06/2018.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {SyncProjectsMongoToSolrCloudJob.class, JobRunnerTestConfiguration.class})
+@TestPropertySource(value = "classpath:application-test.properties")
+@Slf4j
+public class SyncProjectsMongoToSolrCLoudJobTest {
+
+        @Autowired
+        SyncProjectsMongoToSolrCloudJob mongoToSolrCloudJob;
+
+        @Autowired
+        private JobLauncherTestUtils jobLauncherTestUtils;
+
+        /**
+         * This test should be run during the development process
+         * @throws Exception
+         */
+        @Test
+        public void syncOracleToMongoDB() throws Exception {
+            JobParameters param = new JobParametersBuilder()
+                    .addString("override", "TRUE")
+                    .addString("submissionType", SubmissionPipelineConstants.SubmissionsType.PUBLIC.name())
+                    .toJobParameters();
+            JobExecution jobExecution = jobLauncherTestUtils.launchJob(param);
+            Assert.assertEquals(BatchStatus.COMPLETED.name(), jobExecution.getExitStatus().getExitCode());
+        }
+}
