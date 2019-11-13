d1a8ab559fbd1fb02f7406d5e71c2e527139b7d3
Chakradhar Bandla <chakrabandla@gmail.com>
2019-11-13 13:29:15 +0000

    export annotations as csv: incremental flag : append to existing csv in github

--- scripts/missing_annotations.js
+++ scripts/missing_annotations.js
@@ -3,24 +3,53 @@ const readline = require('readline');
 
 const filename = process.argv[2];
 const csv_out_file = process.argv[3];
+const githubCsvFile = process.argv[4];
+const mode = process.argv[5];
 const csv_header = "ProjectAccession,Instrument,Quantification Method,Software,PTM,Species,Publication_ids";
 
-var csv_writer = fs.createWriteStream(csv_out_file, {
-});
-csv_writer.write(csv_header + "\n");
+const csv_writer = fs.createWriteStream(csv_out_file, {});
+
+var incrementalFlg = false;
+if (mode == "--incremental" || mode == "-i") {
+    incrementalFlg = true;
+}
 
 const readInterface = readline.createInterface({
     input: fs.createReadStream(filename),
     console: false
 });
 
-readInterface.on('line', function (line) {
-    var obj = JSON.parse(line);
-    var csv_line = obj.accession + "," + getAccessions(obj.instruments) + "," + getAccessions(obj.quantificationMethods)
-        + "," + getAccessions(obj.softwareList) + "," + getAccessions(obj.ptmList) + "," + getSpecies(obj.sample_attributes)
-    + "," + getPublicationIds(obj.project_references);
-    csv_writer.write(csv_line + "\n");
-});
+var githubAccessions = [];
+
+function parseMongoExportFile() {
+    readInterface.on('line', function (line) {
+        let obj = JSON.parse(line);
+        const accession = obj.accession;
+        if((!incrementalFlg) || (incrementalFlg && !githubAccessions.includes(accession))) {
+            let csv_line = accession + "," + getAccessions(obj.instruments) + "," + getAccessions(obj.quantificationMethods)
+                + "," + getAccessions(obj.softwareList) + "," + getAccessions(obj.ptmList) + "," + getSpecies(obj.sample_attributes)
+                + "," + getPublicationIds(obj.project_references);
+            csv_writer.write(csv_line + "\n");
+        }
+    });
+}
+
+if (incrementalFlg) {
+    const githubFileReader = readline.createInterface({
+        input: fs.createReadStream(githubCsvFile),
+        console: false
+    });
+    githubFileReader.on('line', function (line) {
+        githubAccessions.push(line.split(",")[0]);
+        csv_writer.write(line + "\n");
+    });
+    githubFileReader.on('close', function() {
+        parseMongoExportFile()
+    });
+} else {
+    csv_writer.write(csv_header + "\n");
+    parseMongoExportFile();
+}
 
 function getAccessions(obj) {
     var accessions = [];
@@ -38,7 +67,7 @@ function getSpecies(obj) {
         return "";
     }
     obj.forEach(i => {
-        if(i.key.accession == "OBI:0100026") {
+        if (i.key.accession == "OBI:0100026") {
             i.value.forEach(j => species.push(j.accession))
         }
     });
@@ -51,10 +80,10 @@ function getPublicationIds(obj) {
         return "";
     }
     obj.forEach(i => {
-        if(i.pubmedID) {
+        if (i.pubmedID) {
             ids.push("PMID:" + i.pubmedID);
         }
-        if(i.doi) {
+        if (i.doi) {
             ids.push("doi:" + i.doi);
         }
     });
--- scripts/projects_missing_annotations.sh
+++ scripts/projects_missing_annotations.sh
@@ -1,17 +1,20 @@
-DATE=$(date +"%Y%m%d%H%M")
+DATE=$(date +"%Y%m%d_%H%M")
 EXPORT_JSON_FILE_NAME="pride_projects-${DATE}.json"
 EXPORT_CSV_FILE_NAME="pride_projects-${DATE}.csv"
+GITHUB_CSV_FILE_NAME="github_file.csv"
 
 EXPORT_JSON_FILE=$EXPORT_PATH/${EXPORT_JSON_FILE_NAME}
 EXPORT_CSV_FILE=$EXPORT_PATH/${EXPORT_CSV_FILE_NAME}
+GITHUB_CSV_FILE=$EXPORT_PATH/${GITHUB_CSV_FILE_NAME}
 
 mkdir -p $EXPORT_PATH
 
 ${MONGO_BIN_PATH}/mongoexport --uri="$mongodb_machine_uri" --collection=pride_projects --out="${EXPORT_JSON_FILE}" --fields='accession,instruments,quantificationMethods,softwareList,ptmList,sample_attributes,project_references'
 
-${NODE_PATH}/node missing_annotations.js ${EXPORT_JSON_FILE} ${EXPORT_CSV_FILE}
+wget https://raw.githubusercontent.com/PRIDE-Utilities/pride-ontology/master/pride-annotations/pride_projects.csv  -O ${GITHUB_CSV_FILE}
+
+${NODE_PATH}/node missing_annotations.js ${EXPORT_JSON_FILE} ${EXPORT_CSV_FILE} ${GITHUB_CSV_FILE} "$@"
 
 curl -F file=@${EXPORT_CSV_FILE} -F "initial_comment=Missing annotations: ${EXPORT_CSV_FILE_NAME}" -F channels=review -H "Authorization: Bearer ${SLACK_APP_TOKEN}" https://slack.com/api/files.upload
 
-rm ${EXPORT_JSON_FILE} ${EXPORT_CSV_FILE}
-
+rm ${EXPORT_JSON_FILE} ${EXPORT_CSV_FILE} ${GITHUB_CSV_FILE}
